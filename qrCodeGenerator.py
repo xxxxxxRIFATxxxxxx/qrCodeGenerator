ebbb529b7d1679da4c8b83fdce44115555e32eb6
Md. Rifat Islam <xxxxrifatxxxx@gmail.com>
2023-10-25 17:06:32 +0600

    done

--- qrCodeGenerator.py
+++ qrCodeGenerator.py
@@ -7,3 +7,16 @@ img = qr.make(data)
 
 # Saving as an image file
 img.save('mayan.png')
+
+# import json
+# import qrcode as qr
+
+# # Load the JSON data
+# with open('./100_guest.json', 'r') as file:
+#     data = json.load(file)
+
+# # Assuming your JSON structure is a list of dictionaries with an 'id' key:
+# # Example: [{"id": "https://example1.com"}, {"id": "https://example2.com"}, ...]
+# for index, item in enumerate(data, start=1):
+#     img = qr.make(item['id'])
+#     img.save(f'{index}.png')
